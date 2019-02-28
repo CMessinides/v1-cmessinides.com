909becff410e4cc6b00a6b6c3d7ef31e2ecb996f
Cameron Messinides <cameron.messinides@gmail.com>
2019-02-27 22:26:20 -0500

    Remove default header component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,44 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header";
 import "./index.scss";
 import LightboxProvider, { LightboxContext } from "./lightbox";
 
 const Layout = ({ children }) => {
   return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => (
-        <LightboxProvider>
-          <LightboxContext.Consumer>
-            {lightbox => (
-              <div
-                className="application"
-                aria-hidden={lightbox.isOpen ? "true" : null}
-              >
-                <Header siteTitle={data.site.siteMetadata.title} />
-                <main>{children}</main>
-                <footer>
-                  © {new Date().getFullYear()}, Built with
-                  {` `}
-                  <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </footer>
-              </div>
-            )}
-          </LightboxContext.Consumer>
-        </LightboxProvider>
-      )}
-    />
+    <LightboxProvider>
+      <LightboxContext.Consumer>
+        {lightbox => (
+          <div
+            className="application"
+            aria-hidden={lightbox.isOpen ? "true" : null}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        )}
+      </LightboxContext.Consumer>
+    </LightboxProvider>
   );
 };
 
